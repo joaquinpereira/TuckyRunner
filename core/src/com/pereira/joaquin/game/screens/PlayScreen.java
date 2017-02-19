0f5037417fbe138fa4c4ae91abc21644845e900f
Joaquin Pereira <pereira.joaquin@gmail.com>
2017-02-19 10:49:54 -0430

    primer commit

---
+++ core/src/com/pereira/joaquin/game/screens/PlayScreen.java
@@ -0,0 +1,76 @@
+package com.pereira.joaquin.game.screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.pereira.joaquin.game.MainClass;
+
+/**
+ * Created by Usuario on 18/02/2017.
+ */
+
+public class PlayScreen implements Screen {
+
+    private TextureAtlas atlas;
+
+    //basic play screen variables
+    private OrthographicCamera gameCam;
+    private Viewport gamePort;
+//    private Hud hud;
+
+    //tiled map variables
+    private TmxMapLoader mapLoader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+
+    //Box2d variables
+    private World world;
+    private Box2DDebugRenderer b2dr;
+//    private B2WorldCreator creator;
+
+    public PlayScreen(MainClass game){
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
